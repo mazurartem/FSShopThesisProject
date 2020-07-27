22913048c327796fc79fed2e5bdfb7f969f6b794
mazurartem <61084254+mazurartem@users.noreply.github.com>
2020-07-27 20:22:01 +0300

    add

---
+++ src/js/map.js
@@ -0,0 +1,11 @@
+var map = L.map("map").setView([0, 0], 1);
+
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=EA3jla9XBTGO9g76nilk",
+  {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(map);
+
+var marker = L.marker([49.068, 33.4204]).addTo(map);
